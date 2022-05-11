229fd5db77b3328d217bddb24dda55984286d006
ismail <ismailgim96@gmail.com>
2022-05-11 15:31:16 +0200

    Adding new validation features with React Hook Form

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,20 +1,14 @@
 import React, { Fragment } from "react";
-import { Form, Button, Alert, Col, Row} from "react-bootstrap";
+import { Form, Button, Alert, Col, Row } from "react-bootstrap";
 import { useMutation } from "react-query";
 import { login } from "../services/user";
 import { useRef } from "react";
 import { Container } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
-import NavB from "../components/Navbar"
+import NavB from "../components/Navbar";
 import { useForm } from "react-hook-form";
-import { yupResolver } from '@hookform/resolvers/yup';
+import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
-
-const schema = yup.object({
-  email: yup.string().required(),
-  password: yup.string().min(8).max(32).required(),
-}).required();
-
 
 
 const Login = () => {
@@ -22,73 +16,82 @@ const Login = () => {
   const emailInputRef = useRef();
   const passwordInputRef = useRef();
 
-  const { register, handleSubmit, formState:{ errors } } = useForm({
-    resolver: yupResolver(schema)
+  const schema = yup
+  .object({
+    email: yup.string().required(),
+    password: yup.string().required('No password provided.').min(8, 'Password is too short - should be 8 chars minimum.').matches(/[a-zA-Z]/, 'Password can only contain Latin letters.')
+  })
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
   });
 
   const mutation = useMutation((info) => login(info));
 
-
   const onSubmitHandler = () => {
     mutation.mutate({
-      email: emailInputRef.current.value,
-      password: passwordInputRef.current.value,
+      Email: emailInputRef.current.value,
+      Password: passwordInputRef.current.value,
     });
-    }
-  
-
+  };
 
   return (
     <Fragment>
-      <div><NavB></NavB></div>
-    <Container className="p-3">
-      <Container className="p-5 mb-4 bg-light rounded-3">
-        <Form onSubmit={onSubmitHandler}>
-          <Row>
-          <Col>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control
-              {...register("email")} placeholder="email" type="email" required
-              ref={emailInputRef}
-            />
-            <Form.Text className="text-muted">
-            {errors.email?.message}
-            </Form.Text>
-          </Form.Group>
-          </Col>
-          <Col>
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control
-              {...register("password")}
-              placeholder="password"
-              type="password"
-              required
-              ref={passwordInputRef}
-            />
-            <Form.Text className="text-muted">
-            {errors.password?.message}
-            </Form.Text>
-          </Form.Group>
-          </Col>
-          </Row>
-          <Button
-            type='submit'
-          >
-            Login
-          </Button>
-          <Alert key={"light"} variant={"light"}>
-            If u dont have an account
-          </Alert>
-          <Button type="submit" onClick={() => navigate("/Register")}>
-            Sign up
-          </Button>
-        </Form>
+      <div>
+        <NavB></NavB>
+      </div>
+      <Container className="p-3">
+        <Container className="p-5 mb-4 bg-light rounded-3">
+          <Form onSubmit={onSubmitHandler}>
+            <Row>
+              <Col>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <Form.Label>Email address</Form.Label>
+                  <Form.Control
+                    {...register("email")}
+                    placeholder="email"
+                    name="email"
+                    type="email"
+                    required
+                    ref={emailInputRef}
+                  />
+                  <Form.Text className="text-muted">
+                    {errors.email?.message}
+                  </Form.Text>
+                </Form.Group>
+              </Col>
+              <Col>
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control
+                    {...register("password")}
+                    placeholder="password"
+                    name="password"
+                    type="password"
+                    required
+                    ref={passwordInputRef}
+                  />
+                  <Form.Text className="text-muted">
+                    {errors.password?.message}
+                  </Form.Text>
+                </Form.Group>
+              </Col>
+            </Row>
+            <Button type="submit">Login</Button>
+            <Alert key={"light"} variant={"light"}>
+              If u dont have an account
+            </Alert>
+            <Button type="submit" onClick={() => navigate("/Register")}>
+              Sign up
+            </Button>
+          </Form>
+        </Container>
       </Container>
-    </Container>
     </Fragment>
-    
   );
 };
 

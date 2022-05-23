0b89a9b23f8eca1d6b26ccd128beb085760357c5
ismail <ismailgim96@gmail.com>
2022-05-23 14:32:13 +0200

    Improving navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,51 @@
-import React from "react";
-import { Nav } from "react-bootstrap";
+import React, { useState } from "react";
+
+import styles from "./Header.module.scss";
+
+import { BiMenuAltRight } from "react-icons/bi";
+import { AiOutlineCloseSquare } from "react-icons/ai";
 
 const NavB = () => {
-  return (
-    <Nav fill variant="tabs" defaultActiveKey="/">
-      <Nav.Item>
-        <Nav.Link href="/">Home</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link href="books">books</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link href="houses">houses</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link href="login">login</Nav.Link>
-      </Nav.Item>
-    </Nav>
-  );
+    const [menuOpen, setMenuOpen] = useState(false);
+    const menuToggler = () => setMenuOpen((p) => !p);
+
+    return (
+        <div className={styles.header}>
+            <div className={styles.header__content}>
+                <div>
+                    <span className={styles.logo}>Code Focus</span>
+                </div>
+                <div>
+                    <nav className={`${styles.nav} ${menuOpen ? styles[`nav--open`] : {}}`}>
+                        <a className={styles.nav__item} href={"/"}>
+                            Page One
+                        </a>
+                        <a className={styles.nav__item} href={"/"}>
+                            Page Two
+                        </a>
+                        <a className={styles.nav__item} href={"/"}>
+                            Page Three
+                        </a>
+                        <div className={styles.nav__button__container}>
+                            <Button />
+                        </div>
+                    </nav>
+                </div>
+                <div>
+                    <div className={styles.header__button__container}>
+                        <Button />
+                    </div>
+                    <button className={styles.header__toggler} onClick={menuToggler}>
+                        {!menuOpen ? <BiMenuAltRight /> : <AiOutlineCloseSquare />}
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+const Button = () => {
+    return <button className={styles.button}>Click me</button>;
 };
 
 export default NavB;

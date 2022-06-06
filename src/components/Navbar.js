3e3fcc23a49cf8be146c9a0f8122dc121a2db135
ismail <ismailgim96@gmail.com>
2022-06-06 13:10:33 +0200

    navbar changed,products content changed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
-import "./Header.scss";
 import { store } from '../store'
 import { userActions } from '../store/userSlice';
 import { useNavigate } from "react-router-dom";
 import { Button } from 'react-bootstrap'
+import { Navbar, Form, FormControl, Nav, NavDropdown, Container } from "react-bootstrap";
 
 
 
@@ -18,29 +18,50 @@ const NavB = () => {
 
 
   return (
-    <div className="header">
-      <div className="header__content">
-        <div>
-          <span className="logo">Code Focus</span>
-        </div>
-        <div>
-          <nav className="nav">
-            <a className="nav__item" href={"/"}>
-              home
-            </a>
-            <a className="nav__item" href={"/products"}>
-              products
-            </a>
+    <Navbar bg="light" expand="lg">
+  <Container fluid>
+    <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+    <Navbar.Toggle aria-controls="navbarScroll" />
+    <Navbar.Collapse id="navbarScroll">
+      <Nav
+        className="me-auto my-2 my-lg-0"
+        style={{ maxHeight: '100px' }}
+        navbarScroll
+      >
+        <Nav.Link href="homepage">Home</Nav.Link>
+        {Loggedin ? (<Nav.Link onClick={logOutHandler}>Logout</Nav.Link>):null}
+        
+        <NavDropdown title="Link" id="navbarScrollingDropdown">
+          <NavDropdown.Item href="laptops">Laptops</NavDropdown.Item>
+          <NavDropdown.Item href="books">Tablets</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href="products">
+            Products
+          </NavDropdown.Item>
+        </NavDropdown>
+        <Nav.Link href="#" disabled>
+          Link
+        </Nav.Link>
+      </Nav>
+      <Form className="d-flex">
+        <FormControl
+          type="search"
+          placeholder="Search"
+          className="me-2"
+          aria-label="Search"
+        />
+        <Button variant="outline-success">Search</Button>
+      </Form>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
 
-            {Loggedin ? (<Button onClick={logOutHandler}>
-              logout 
-            </Button>):null}
-          </nav>
-        </div>
-        <div></div>
-      </div>
-    </div>
+
+
+    
   );
 };
+
+
 
 export default NavB;
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,25 +1,24 @@
 import React from "react";
+import { Fragment } from "react";
 import { useQuery } from "react-query";
 import { getProducts } from "../services/products";
 import ProductsContainer from "./LaptopContainer";
 import useProduct from "../customHooks/ProductsHook";
-import {Container, Row,Col} from 'react-bootstrap'
+import { Container, Row, Col } from "react-bootstrap";
+import Laptops from "./Laptops";
+import Tablets from "./Tablets";
 
 function Products(props) {
   const { data: Products = [] } = useQuery("getProducts", () => getProducts());
   const [products] = useProduct(Products);
 
   return (
-    <div className="wrapper">
-      {products.map((prod) => (
-        <ProductsContainer
-          key={prod.id}
-          id={prod.id}
-          pricee={prod.cena}
-          imagee={prod.image}
-        />
-      ))}
-    </div>
+    <Fragment>
+      <div className='wrapper'>
+      <Laptops/>
+      <Tablets/>
+      </div>
+    </Fragment>
   );
 }
 

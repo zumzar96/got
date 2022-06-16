48184d743457b6979305619710c4aaa5c5677ec9
ismail <ismailgim96@gmail.com>
2022-06-16 13:06:36 +0200

    Adding dependency to useEffect in SearchProduct component

--- src/pages/SearchProduct.js
+++ src/pages/SearchProduct.js
@@ -6,29 +6,40 @@ import { getProduct } from "../services/products";
 import { Fragment } from "react";
 import { useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const SearchProduct = (props) => {
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
   const { state } = useLocation();
   const { searchedProd } = state; // Read values passed on state
   const { data: product = [], refetch } = useQuery("getProduct", () =>
     getProduct(state.searchedProd)
   );
+
   useEffect(() => {
     refetch();
-  }, [searchedProd]);
+  }, [refetch()]);
+
   return (
-    <Container>
-      <Row>
-        <Col>
-          <Crd
-            style={{ width: "13rem" }}
-            src={product.image}
-            price={product.cena}
-            id={state.searchedProd}
-          ></Crd>
-        </Col>
-      </Row>
-    </Container>
+    <Fragment>
+      {product !== null ? (
+        <Container>
+          <Row>
+            <Col>
+              <Crd
+                style={{ width: "13rem" }}
+                src={product?.image}
+                price={product?.cena}
+                id={state?.searchedProd}
+                onClick={props.onClick}
+              ></Crd>
+            </Col>
+          </Row>
+        </Container>
+      ) : null}
+    </Fragment>
   );
 };
 
